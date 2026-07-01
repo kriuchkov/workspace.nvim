7e5b4e6580a42cc63e3fc15b29c3857fb7bed4b7
Nikita Kriuchkov <nkriuchkov@outlook.com>
2026-07-01 18:23:18 +0300

    feat: <leader>uw toggles word wrap (wrap+linebreak+breakindent)

--- lua/claudespace/keymaps.lua
+++ lua/claudespace/keymaps.lua
@@ -29,5 +29,14 @@ end, { desc = 'Prev LSP reference' })
 -- Terminal: exit with double Esc
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Toggle word wrap (word-boundary breaks + kept indent) for the current window
+map('n', '<leader>uw', function()
+  local on = not vim.wo.wrap
+  vim.wo.wrap        = on
+  vim.wo.linebreak   = on
+  vim.wo.breakindent = on
+  vim.notify('Word wrap ' .. (on and 'on' or 'off'), vim.log.levels.INFO)
+end, { desc = 'Toggle word wrap' })
+
 -- Clear search highlight
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
